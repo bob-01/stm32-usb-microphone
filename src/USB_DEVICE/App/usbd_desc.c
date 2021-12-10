7a203c578bd579a416928ef0391e5a7651ce64f4
bob <alexxx.iv@yandex.ru>
2021-12-11 02:10:03 +0300

    123

--- src/USB_DEVICE/App/usbd_desc.c
+++ src/USB_DEVICE/App/usbd_desc.c
@@ -65,7 +65,7 @@
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     5730
+#define USBD_PID_FS     22336
 #define USBD_PRODUCT_STRING_FS     "STM32 Audio Class"
 #define USBD_CONFIGURATION_STRING_FS     "AUDIO Config"
 #define USBD_INTERFACE_STRING_FS     "AUDIO Interface"
--- src/USB_DEVICE/Target/usbd_conf.h
+++ src/USB_DEVICE/Target/usbd_conf.h
@@ -62,7 +62,7 @@
   */
 
 /*---------- -----------*/
-#define USBD_MAX_NUM_INTERFACES     2
+#define USBD_MAX_NUM_INTERFACES     5
 /*---------- -----------*/
 #define USBD_MAX_NUM_CONFIGURATION     1
 /*---------- -----------*/
@@ -70,7 +70,7 @@
 /*---------- -----------*/
 #define USBD_DEBUG_LEVEL     0
 /*---------- -----------*/
-#define USBD_SELF_POWERED     1
+#define USBD_SELF_POWERED     0
 /*---------- -----------*/
 #define USBD_AUDIO_FREQ     48000
 
